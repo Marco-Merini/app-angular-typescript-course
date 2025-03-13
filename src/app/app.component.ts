1ae295abd00b246eb80284f067a482d3a0ea60e2
Marco-Merini <marcoleonemerini@gmail.com>
2025-03-13 14:03:20 -0300

    Adicionando componentes iniciais e styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopoComponent } from './topo/topo.component';
+import { PainelComponent } from "./painel/painel.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopoComponent, PainelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/painel/painel.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ProgressoComponent } from "../progresso/progresso.component";
+import { TentativasComponent } from "../tentativas/tentativas.component";
+
+@Component({
+  selector: 'app-painel',
+  imports: [ProgressoComponent, TentativasComponent],
+  templateUrl: './painel.component.html',
+  styleUrl: './painel.component.css'
+})
+export class PainelComponent implements OnInit {
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    
+  }
+
+}
